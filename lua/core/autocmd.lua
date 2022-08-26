3fb0b4b95b72063d436b45c376b4effc58d067ee
Ruixi-rebirth <ruixirebirth@gmail.com>
2022-08-26 10:55:48 +0800

    first commit

---
+++ lua/core/autocmd.lua
@@ -0,0 +1,14 @@
+--Revert to where the cursor was when the file was last closed
+vim.cmd([[autocmd BufReadPost *
+    \ if line("'\"") > 1 && line("'\"") <= line("$") |
+    \   exe "normal! g`\"" |
+    \ endif]])
+
+vim.cmd([[set iskeyword+=-]])
+
+vim.cmd("set whichwrap+=<,>,[,],h,l")
+
+-- about fold 
+vim.cmd("set foldmethod=expr")
+vim.cmd("set foldexpr=nvim_treesitter#foldexpr()")
+

0326798cbe08a1f1624f5d956a73e99fa4a6bd7e
Ruixi-rebirth <ruixirebirth@gmail.com>
2022-09-24 01:34:54 +0800

    deal some details

--- lua/ui/colorscheme.lua
+++ lua/ui/colorscheme.lua
@@ -1,4 +1,4 @@
-local colorscheme = "catppuccin" --[nord | catppuccin]
+local colorscheme = "catppuccin" 
 
 local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
 if not status_ok then
